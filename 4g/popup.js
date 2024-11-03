9a4b788bd1c78b84b4c41c32d604cdba2e7e7335
Vít Repka <143121243+VitoIV@users.noreply.github.com>
2024-11-03 17:12:18 +0100

    Added product preview
    Added product preview

--- 4g/popup.js
+++ 4g/popup.js
@@ -11,33 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const imgOpen = document.getElementById('imgOpen');
   const imgCategory = document.getElementById('imgCategory');
   const inputText = document.getElementById('inputText');
-  const btnPreview = document.getElementById('btnPreview');
-  const imgPreview = document.getElementById('imgPreview');
 
   let keepOn = localStorage.getItem('keepOn') === 'true';
   let openOn = localStorage.getItem('openOn') === 'true';
-  console.log(`Loaded openOn from localStorage: ${openOn}`);
   let categoryOn = localStorage.getItem('categoryOn') === 'true';
-
-  let editDisabled = localStorage.getItem('editDisabled') === 'true'; 
-  let imageDisabled = localStorage.getItem('imageDisabled') === 'true'; 
-  let pDisabled = localStorage.getItem('pDisabled') === 'true'; 
-
-  let previewOn = localStorage.getItem('previewOn') === 'true';
-  
-  btnEdit.disabled = editDisabled; 
-  btnP.disabled = pDisabled; 
-  btnImage.disabled = imageDisabled; 
-
-  if (editDisabled) { 
-    btnEdit.classList.add('disabled'); 
-  } 
-  if (pDisabled) { 
-    btnP.classList.add('disabled'); 
-  } 
-  if (imageDisabled) { 
-    btnImage.classList.add('disabled'); 
-  } 
 
   if (keepOn && openOn) {
       keepOn = false;
@@ -50,77 +27,10 @@ document.addEventListener('DOMContentLoaded', function() {
   updateButtonState(btnKeep, imgKeep, keepOn, 'icons/keep.png', 'icons/keep_off.png');
   updateButtonState(btnOpen, imgOpen, openOn, 'icons/open.png', 'icons/open_off.png');
   updateButtonState(btnCategory, imgCategory, categoryOn, 'icons/category.png', 'icons/category.png');
-  updateButtonsStateCategory(categoryOn); 
-  updateButtonState(btnPreview, imgPreview, previewOn, 'icons/preview_on.png', 'icons/preview_off.png');
-
-  
-  if (!categoryOn) {
-    updateEditAndPState(openOn); 
-  }
-
+  updateEditAndPState(openOn);
+  updateButtonsStateCategory(categoryOn);
 
   console.log(`Initial KeepOn: ${keepOn}, OpenOn: ${openOn}, CategoryOn: ${categoryOn}`);
-
-  inputText.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        console.log('Enter key pressed. Triggering CZ button click.');
-        document.getElementById('btnCZ').click();
-    }
-});
-
-btnPreview.addEventListener('click', function() {
-  console.log('Preview button clicked.');
-  previewOn = !previewOn;
-
-  if (previewOn) {
-      console.log('Preview mode is now ON');
-      
-      // Aktivace připnutí při zapnutí preview
-      keepOn = true;
-      updateButtonState(btnKeep, imgKeep, keepOn, 'icons/keep.png', 'icons/keep_off.png');
-      localStorage.setItem('keepOn', 'true');
-      console.log('Keep mode enabled as a requirement for Preview mode.');
-
-      // Deaktivace categoryOn, pokud se zapne previewOn
-      categoryOn = false;
-      updateButtonState(btnCategory, imgCategory, categoryOn, 'icons/category.png', 'icons/category.png');
-      localStorage.setItem('categoryOn', 'false');
-      console.log('Category mode disabled when preview mode is ON.');
-
-      // Zamezení úprav v galerii, parametrech a editaci
-      btnEdit.classList.add('disabled');
-      btnImage.classList.add('disabled');
-      btnP.classList.add('disabled');
-      btnEdit.disabled = true;
-      btnImage.disabled = true;
-      btnP.disabled = true;
-      localStorage.setItem('editDisabled', 'true');
-      localStorage.setItem('imageDisabled', 'true');
-      localStorage.setItem('pDisabled', 'true');
-  } else {
-      console.log('Preview mode is now OFF');
-      
-      // Obnovení původního stavu připnutí, pokud uživatel vypne preview
-      keepOn = false;
-      updateButtonState(btnKeep, imgKeep, keepOn, 'icons/keep.png', 'icons/keep_off.png');
-      localStorage.setItem('keepOn', 'false');
-      console.log('Keep mode disabled because Preview mode is OFF.');
-
-      // Obnovení původního stavu tlačítek při vypnutí preview
-      btnEdit.classList.remove('disabled');
-      btnImage.classList.remove('disabled');
-      btnP.classList.remove('disabled');
-      btnEdit.disabled = false;
-      btnImage.disabled = false;
-      btnP.disabled = false;
-      localStorage.setItem('editDisabled', 'false');
-      localStorage.setItem('imageDisabled', 'false');
-      localStorage.setItem('pDisabled', 'false');
-  }
-
-  updateButtonState(btnPreview, imgPreview, previewOn, 'icons/preview_on.png', 'icons/preview_off.png');
-  localStorage.setItem('previewOn', previewOn ? 'true' : 'false');
-});
 
   btnKeep.addEventListener('click', function() {
       console.log('Keep button clicked.');
@@ -138,93 +48,53 @@ btnPreview.addEventListener('click', function() {
   });
 
   btnOpen.addEventListener('click', function() {
-    console.log('Open button clicked.');
-    openOn = !openOn;
-    if (openOn) {
-        keepOn = false;
-        updateButtonState(btnKeep, imgKeep, keepOn, 'icons/keep.png', 'icons/keep_off.png');
-        localStorage.setItem('keepOn', 'false');
-        console.log('Open button turned on, Keep button turned off.');
-    } else {
-        console.log('Open button turned off.');
-    }
-    updateButtonState(btnOpen, imgOpen, openOn, 'icons/open.png', 'icons/open_off.png');
-    localStorage.setItem('openOn', openOn ? 'true' : 'false');
-
-    
-    if (!categoryOn) {
+      console.log('Open button clicked.');
+      openOn = !openOn;
+      if (openOn) {
+          keepOn = false;
+          updateButtonState(btnKeep, imgKeep, keepOn, 'icons/keep.png', 'icons/keep_off.png');
+          localStorage.setItem('keepOn', 'false');
+          console.log('Open button turned on, Keep button turned off.');
+      } else {
+          console.log('Open button turned off.');
+      }
+      updateButtonState(btnOpen, imgOpen, openOn, 'icons/open.png', 'icons/open_off.png');
+      localStorage.setItem('openOn', openOn ? 'true' : 'false');
       updateEditAndPState(openOn);
-    }
-});
+  });
 
-btnCategory.addEventListener('click', function() {
-  console.log('Category button clicked.');
-  categoryOn = !categoryOn;
-
-  if (categoryOn) {
-      // Deaktivace previewOn, pokud se zapne categoryOn
-      previewOn = false;
-      updateButtonState(btnPreview, imgPreview, previewOn, 'icons/preview_on.png', 'icons/preview_off.png');
-      localStorage.setItem('previewOn', 'false');
-      console.log('Category mode enabled, Preview mode disabled.');
-
-      // Zamezení úprav v galerii, parametrech a editaci
-      btnEdit.classList.add('disabled');
-      btnImage.classList.add('disabled');
-      btnP.classList.add('disabled');
-      btnEdit.disabled = true;
-      btnImage.disabled = true;
-      btnP.disabled = true;
-      localStorage.setItem('editDisabled', 'true');
-      localStorage.setItem('imageDisabled', 'true');
-      localStorage.setItem('pDisabled', 'true');
-  } else {
-      // Obnovení původního stavu tlačítek při vypnutí category
-      btnEdit.classList.remove('disabled');
-      btnImage.classList.remove('disabled');
-      btnP.classList.remove('disabled');
-      btnEdit.disabled = false;
-      btnImage.disabled = false;
-      btnP.disabled = false;
-      localStorage.setItem('editDisabled', 'false');
-      localStorage.setItem('imageDisabled', 'false');
-      localStorage.setItem('pDisabled', 'false');
-      console.log('Category mode disabled, restored previous button states.');
-  }
-
-  updateButtonState(btnCategory, imgCategory, categoryOn, 'icons/category.png', 'icons/category.png');
-  localStorage.setItem('categoryOn', categoryOn ? 'true' : 'false');
-});
+  btnCategory.addEventListener('click', function() {
+      console.log('Category button clicked.');
+      categoryOn = !categoryOn;
+      updateButtonState(btnCategory, imgCategory, categoryOn, 'icons/category.png', 'icons/category.png');
+      localStorage.setItem('categoryOn', categoryOn ? 'true' : 'false');
+      console.log(`Category button turned ${categoryOn ? 'on' : 'off'}.`);
+      updateButtonsStateCategory(categoryOn);
+  });
 
   function updateButtonState(button, img, isOn, onSrc, offSrc) {
       console.log(`Updating button state: ${button.id}, State: ${isOn}`);
       button.classList.toggle('on', isOn);
       button.classList.toggle('off', !isOn);
-      img.src = isOn ? onSrc : offSrc;
+      img.src = onSrc;  
   }
 
   function updateEditAndPState(openOn) {
       console.log(`Updating Edit and P button states. OpenOn: ${openOn}`);
       if (openOn) {
-          console.log('Disabling Edit and P buttons.');
           btnEdit.classList.add('disabled');
           btnP.classList.add('disabled');
           btnEdit.disabled = true;
           btnP.disabled = true;
           btnEdit.title = "Nejde použít v novém okně";
           btnP.title = "Nejde použít v novém okně";
-          localStorage.setItem('editDisabled', 'true'); 
-          localStorage.setItem('pDisabled', 'true'); 
       } else {
-          console.log('Enabling Edit and P buttons.');
           btnEdit.classList.remove('disabled');
           btnP.classList.remove('disabled');
           btnEdit.disabled = false;
           btnP.disabled = false;
           btnEdit.title = "";
           btnP.title = "";
-          localStorage.setItem('editDisabled', 'false'); 
-          localStorage.setItem('pDisabled', 'false'); 
       }
   }
 
@@ -240,9 +110,6 @@ btnCategory.addEventListener('click', function() {
           btnEdit.title = "Nejdřív vypni otvírání kategorií";
           btnImage.title = "Nejdřív vypni otvírání kategorií";
           btnP.title = "Nejdřív vypni otvírání kategorií";
-          localStorage.setItem('editDisabled', 'true'); 
-          localStorage.setItem('imageDisabled', 'true'); 
-          localStorage.setItem('pDisabled', 'true'); 
       } else {
           btnEdit.classList.remove('disabled');
           btnImage.classList.remove('disabled');
@@ -253,29 +120,20 @@ btnCategory.addEventListener('click', function() {
           btnEdit.title = "";
           btnImage.title = "";
           btnP.title = "";
-          localStorage.setItem('editDisabled', 'false'); 
-          localStorage.setItem('imageDisabled', 'false'); 
-          localStorage.setItem('pDisabled', 'false'); 
       }
   }
 
   function handleButtonClick(action) {
-    console.log('Handling button click. PreviewOn:', previewOn); // Výpis pro kontrolu
-
-    if (previewOn) {
-        redirectToPreview(); // Volání preview funkce
-    } else {
-        action(); // Původní přesměrování
-    }
-
-    const openInNewTab = localStorage.getItem('openOn') === 'true';
-    if (!keepOn && !openInNewTab) {
-        console.log('Closing extension window.');
-        window.close();
-    } else if (keepOn) {
-        console.log('Keeping window open due to keepOn being true.');
-    }
-}
+      console.log('Handling button click.');
+      action();
+      const openInNewTab = localStorage.getItem('openOn') === 'true';
+      if (!keepOn && !openInNewTab) {
+          console.log('Closing extension window.');
+          window.close();
+      } else if (keepOn) {
+          console.log('Keeping window open due to keepOn being true.');
+      }
+  }
 
   function redirectTo(tld) {
       const inputTextValue = document.getElementById('inputText').value.trim();
@@ -447,53 +305,7 @@ btnCategory.addEventListener('click', function() {
     }
   }
 
-  function redirectToPreview() {
-    const inputTextValue = document.getElementById('inputText').value.trim();
-    console.log(`Redirecting to preview. Input: ${inputTextValue}`);
-
-    if (inputTextValue) {
-        const initialUrl = `https://alza.cz/kod/${inputTextValue}`;
-        const openInNewTab = localStorage.getItem('openOn') === 'true';
-
-        console.log(`Redirecting to initial URL: ${initialUrl}`);
-
-        const handlePreviewRedirection = (tabId) => {
-            chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
-                if (changeInfo.status === 'complete') {
-                    chrome.tabs.get(tabId, function(tab) {
-                        const url = tab.url;
-                        const match = url.match(/[?&]dq=(\d+)/);
-                        console.log(url, match);
-                        if (match) {
-                            const dqValue = match[1];
-                            const previewUrl = `${url.split('?')[0]}?dpgpreview=${dqValue}&dq=${dqValue}`;
-                            console.log(`Redirecting to preview URL: ${previewUrl}`);
-                            chrome.tabs.update(tabId, { url: previewUrl });
-                            chrome.tabs.onUpdated.removeListener(listener);
-                        }
-                    });
-                }
-            });
-        };
-
-        if (openInNewTab) {
-            chrome.tabs.create({ url: initialUrl }, function(tab) {
-                handlePreviewRedirection(tab.id);
-            });
-        } else {
-            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-                const currentTabId = tabs[0].id;
-                chrome.tabs.update(currentTabId, { url: initialUrl }, function() {
-                    handlePreviewRedirection(currentTabId);
-                });
-            });
-        }
-    } else {
-        alert('První zadej kód produktu.');
-    }
-}
-
-   fetch(chrome.runtime.getURL('version.txt'))
+  /* fetch(chrome.runtime.getURL('version.txt'))
     .then(response => response.text())
     .then(currentVersion => {
       currentVersion = currentVersion.trim();
@@ -550,5 +362,5 @@ btnCategory.addEventListener('click', function() {
 
   if (!hideNotification) {
     console.log('Displaying notification because it is not hidden.'); 
-  }
+  }*/
 });
